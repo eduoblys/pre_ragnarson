b2c6f8f23be3c036db26da36bc3bc6666effa7f3
eduoblys <eduoblys@gmail.com>
2021-10-21 10:54:28 +0200

    final commit before starting on real web

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Footer = props => (
 <div class="bg-gray-100 fixed bottom-0 w-full">
-      <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row  sticky bottom-0">
+      <div class="container mx-auto py-3 px-5 flex flex-wrap flex-col sm:flex-row  sticky bottom-0">
         <p class="text-gray-500 text-center text-sm">© 2021
 
         </p>
